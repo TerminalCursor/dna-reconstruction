f9d66c7ae56335dcb24fa526054fcea58a202062
TerminalCursor <ctcunni1@asu.edu>
2020-07-15 22:28:20 -0700

    ANSI Escape Code Heading

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+/* https://en.wikipedia.org/wiki/ANSI_escape_code */
+
 func main() {
-	fmt.Printf("DNA Reconstruction v00.01\n")
+	fmt.Printf("\033[91mDNA\033[0m \033[94mReconstruction\033[0m \033[33mv00.01\033[0m\n")
 }
