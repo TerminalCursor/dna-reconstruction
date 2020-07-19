7a0ab09253b15c50486a752bb967a6685a1b0ab5
TerminalCursor <ctcunni1@asu.edu>
2020-07-18 21:57:49 -0700

    Added optimal strand finding by sliding staple strand starting position

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 func PrintMatch(scaffold, staplePart Strand, offset int) {
 	fmt.Printf("%s\n", scaffold.Bases())
 	for i := 0; i < offset + staplePart.Length(); i++ {
-		if i >= offset {
-			fmt.Printf("%c", staplePart.Bases()[i-offset])
+		if i >= offset { fmt.Printf("%c", staplePart.Bases()[i-offset])
 		} else {
 			fmt.Printf(" ")
 		}
@@ -24,8 +23,7 @@ func GetPermutations(input [][]int, prep []int, interference []int, WINDOW_SIZE 
 			for i := 0; i < len(input[0]); i++ {
 				validPath := true
 				for j := 0; j < len(prep); j++ {
-					if prep[j] <= input[0][i] && input[0][i] < (prep[j] + WINDOW_SIZE) && prep[j] != -1 {
-						validPath = false
+					if prep[j] <= input[0][i] && input[0][i] < (prep[j] + WINDOW_SIZE) && prep[j] != -1 { validPath = false
 						break
 					}
 				}
@@ -69,6 +67,28 @@ func GetPermutations(input [][]int, prep []int, interference []int, WINDOW_SIZE 
 	return out
 }
 
+func GetStaples(input [][]Staple, prep []Staple) [][]Staple {
+	var out [][]Staple
+	if(len(input) > 1) {
+		if len(input[0]) > 0 {
+			for i := 0; i < len(input[0]); i++ {
+				perms := GetStaples(input[1:], append(prep, input[0][i]))
+				for j := 0; j < len(perms); j++ {
+					out = append(out, perms[j])
+				}
+			}
+		} else {
+			fmt.Printf("EMPTY MATCH DETECTED\nTRIVIAL MATCH '-1' NOT INCLUDED\n")
+			os.Exit(1)
+		}
+	} else {
+		for i := 0; i < len(input[0]); i++ {
+			out = append(out, append(prep, input[0][i]))
+		}
+	}
+	return out
+}
+
 func BindingJoin(bindingSites [][]int) []int {
 	var output []int
 	for i := 0; i < len(bindingSites); i++ {
@@ -85,35 +105,62 @@ func main() {
 	fmt.Printf("\033[91mDNA\033[0m \033[94mReconstruction\033[0m \033[33mv00.01\033[0m\n")
 	/* MULTI STAPLES */
 	WINDOW_SIZE := 8
-	staples := []Staple{
-		MakeStaple(MakeStrand("TGGACTCCAACGTCAACCACTATTAAAGAACG"), WINDOW_SIZE),
-		MakeStaple(MakeStrand("GTTCCAGTTTGGAACAAGAGTAGGGCGAAAAACCGTCTATCA"), WINDOW_SIZE),
+	staple_strands := []Strand{
+		MakeStrand("TGGACTCC AACGTCAA CCACTATT AAAGAACG"),
+		MakeStrand("GTTCCAGT TTGGAACA AGAGTAGG GCGAAAAA CCGTCTAT CA"),
 	}
-	m13mp18 := MakeStrand(m13mp18f.Bases()[:TotalLength(staples)]).Reverse()
-	var matchedScaffolds []MatchedScaffold = []MatchedScaffold{MatchedScaffold{m13mp18, []Staple{}, [][]int{}}}
-	var nextMatchedScaffolds []MatchedScaffold
-	for _, staple := range staples {
-		nextMatchedScaffolds = []MatchedScaffold{}
-		var staplePartitionMatches [][]int
-		for i := 0; i < staple.Length(); i++ {
-			staplePartitionMatches = append(staplePartitionMatches, staple.pieces[i].Match(m13mp18))
-			//fmt.Printf("%s %v\n", staple.pieces[i].Bases(), staplePartitionMatches[len(staplePartitionMatches)-1])
-		}
-		//fmt.Printf("%v\n", staplePartitionMatches)
-		var possibleCombinations [][]int
-		for _, ms := range matchedScaffolds {
-			if len(ms.staples) == 0 {
-				possibleCombinations = GetPermutations(staplePartitionMatches, []int{}, []int{}, WINDOW_SIZE)
-			} else {
-				possibleCombinations = GetPermutations(staplePartitionMatches, []int{}, BindingJoin(ms.sbinds), WINDOW_SIZE)
+	// Generate all possible staple subsections of WINDOWSIZE
+	var staple_options [][]Staple
+	for idx, staple_strand := range staple_strands {
+		staple_options = append(staple_options, []Staple{})
+		if int(staple_strand.Length() / 2) >= WINDOW_SIZE {
+			for i := 0; i < WINDOW_SIZE; i++ {
+				staple_options[idx] = append(staple_options[idx], MakeStaple(MakeStrand(staple_strand.Bases()[i:]), WINDOW_SIZE))
 			}
-			for i := 0; i < len(possibleCombinations); i++ {
-				nextMatchedScaffolds = append(nextMatchedScaffolds, MatchedScaffold{m13mp18, append(ms.staples, staple), append(ms.sbinds, possibleCombinations[i])})
-			}
+		} else {
+			fmt.Printf("Cannot generate 2 staples from given sequence at %d windowsize\n", WINDOW_SIZE)
+			os.Exit(1)
 		}
-		matchedScaffolds = nextMatchedScaffolds
 	}
-	for _, matched := range matchedScaffolds {
+	m13mp18 := MakeStrand(m13mp18f.Bases()[:TotalLength(staple_strands)]).Reverse()
+	var topMatch []MatchedScaffold
+	for _, staples := range GetStaples(staple_options, []Staple{}) {
+		var matchedScaffolds []MatchedScaffold = []MatchedScaffold{MatchedScaffold{m13mp18, []Staple{}, [][]int{}}}
+		var nextMatchedScaffolds []MatchedScaffold
+		for _, staple := range staples {
+			nextMatchedScaffolds = []MatchedScaffold{}
+			var staplePartitionMatches [][]int
+			for i := 0; i < staple.Length(); i++ {
+				staplePartitionMatches = append(staplePartitionMatches, staple.pieces[i].Match(m13mp18))
+				//fmt.Printf("%s %v\n", staple.pieces[i].Bases(), staplePartitionMatches[len(staplePartitionMatches)-1])
+			}
+			//fmt.Printf("%v\n", staplePartitionMatches)
+			var possibleCombinations [][]int
+			for _, ms := range matchedScaffolds {
+				if len(ms.staples) == 0 {
+					possibleCombinations = GetPermutations(staplePartitionMatches, []int{}, []int{}, WINDOW_SIZE)
+				} else {
+					possibleCombinations = GetPermutations(staplePartitionMatches, []int{}, BindingJoin(ms.sbinds), WINDOW_SIZE)
+				}
+				for i := 0; i < len(possibleCombinations); i++ {
+					nextMatchedScaffolds = append(nextMatchedScaffolds, MatchedScaffold{m13mp18, append(ms.staples, staple), append(ms.sbinds, possibleCombinations[i])})
+				}
+			}
+			matchedScaffolds = nextMatchedScaffolds
+		}
+		topScore := 0
+		for _, matched := range matchedScaffolds {
+			if topScore < matched.Score() {
+				topScore = matched.Score()
+			}
+		}
+		for _, matched := range matchedScaffolds {
+			if topScore == matched.Score() {
+				topMatch = append(topMatch, matched)
+			}
+		}
+	}
+	for _, matched := range topMatch {
 		fmt.Printf("Match Score: %d\n%s", matched.Score(), matched.MatchedString())
 	}
 }
