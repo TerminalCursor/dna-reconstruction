55139ca4a680d521bf39b9ac7e465b69edc5fcd7
TerminalCursor <ctcunni1@asu.edu>
2020-07-19 22:17:16 -0700

    Cleared up old output of main

--- main.go
+++ main.go
@@ -8,18 +8,13 @@ func main() {
 	fmt.Printf("\033[1;1H\033[2J\033[91mDNA\033[0m \033[94mReconstruction\033[0m \033[33mv00.03\033[0m\n")
 	// Nucleotide Length for Staple Partitions
 	NUCLEOTIDE_LENGTH := 6
+	var staple_strands []Strand
 
 	// Get Staple Strands from staples.txt
 	for _, s := range ReadLines("sample/staples.txt") {
-		fmt.Printf("%s\n", s)
+		staple_strands = append(staple_strands, MakeStrand(s))
+		fmt.Printf("%s\n", staple_strands[len(staple_strands)-1].Bases())
 	}
-
-	fmt.Printf("%c\n", MakeNucleotide('A').base)
-	fmt.Printf("%s\n", MakeStrand("ACTG CAGT").Bases()[:NUCLEOTIDE_LENGTH])
-	strand := MakeStrand("ACTG CAGT")
-	fmt.Printf("%s\n", strand.Bases())
-	fmt.Printf("%s\n", strand.Complement().Bases())
-	fmt.Printf("%s\n", strand.Bases())
 
 	scaffold := Scaffold{
 		MakeStrand("AGTCGTCATGCA"),
