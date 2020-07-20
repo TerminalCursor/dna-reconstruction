7071ab975a61bed629f0914c5f8b3ed516ae5727
TerminalCursor <ctcunni1@asu.edu>
2020-07-19 22:10:08 -0700

    Staple Bonding Example

--- main.go
+++ main.go
@@ -2,16 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
-
-func TestWorker(id int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Printf("Worker %d starting\n", id)
-	time.Sleep(time.Second)
-	fmt.Printf("Worker %d done\n", id)
-}
 
 func main() {
 	fmt.Printf("\033[1;1H\033[2J\033[91mDNA\033[0m \033[94mReconstruction\033[0m \033[33mv00.03\033[0m\n")
@@ -30,15 +21,8 @@ func main() {
 	fmt.Printf("%s\n", strand.Complement().Bases())
 	fmt.Printf("%s\n", strand.Bases())
 
-	var wg sync.WaitGroup
-	for i := 0; i < 20; i++ {
-		wg.Add(1)
-		go TestWorker(i, &wg)
-	}
-	wg.Wait()
-
 	scaffold := Scaffold{
-		MakeStrand("AGTCGTCA"),
+		MakeStrand("AGTCGTCATGCA"),
 		[][]Strand{
 			[]Strand{
 				MakeStrand("TCAG"),
@@ -58,6 +42,8 @@ func main() {
 			fmt.Printf("%v %s\n", scaffold.bonds[sidx][idx], strand.Bases())
 		}
 	}
+	fmt.Printf("%s", scaffold.MatchString())
+	scaffold = scaffold.BondStaple([]Strand{MakeStrand("ACGT"),}, []int{8,})
 	fmt.Printf("%s", scaffold.MatchString())
 	fmt.Printf("%v\n", scaffold.MatchStrand(MakeStrand("GT")))
 }
