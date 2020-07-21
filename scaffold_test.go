408fc2edba3b0fe73f69e0af5e7e792eb27227be
TerminalCursor <ctcunni1@asu.edu>
2020-07-20 21:03:02 -0700

    Added more nuanced test
    Added test to non-scaffold function

--- scaffold_test.go
+++ scaffold_test.go
@@ -24,6 +24,8 @@ func TestMatchStrand(t *testing.T) {
 		want []int
 	}{
 		{Scaffold{MakeStrand("ACTGTGAC"),[][]Strand{},[][]int{}}, MakeStrand("TG"), []int{0, 6,}},
+		{Scaffold{MakeStrand("ACTGTGAC"),[][]Strand{[]Strand{MakeStrand("TG"),},},[][]int{[]int{6,}}}, MakeStrand("TG"), []int{0,}},
+		{Scaffold{MakeStrand("ACTGTGAC"),[][]Strand{[]Strand{MakeStrand("TG"),},},[][]int{[]int{0,}}}, MakeStrand("CTG"), []int{5,}},
 		{Scaffold{MakeStrand("ACTGTGAC"),[][]Strand{},[][]int{}}, MakeStrand("AC"), []int{2, 4,}},
 		{Scaffold{MakeStrand("ACTGTGAC"),[][]Strand{},[][]int{}}, MakeStrand(""), []int{}},
 	}
@@ -33,6 +35,32 @@ func TestMatchStrand(t *testing.T) {
 			ans := tt.sc.MatchStrand(tt.s)
 			if !CompareLists(ans, tt.want) {
 				t.Errorf("got `%v`, want `%v`", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestInIntList(t *testing.T) {
+	var tests = []struct {
+		val int
+		list []int
+		want bool
+	}{
+		{0, []int{0, 1, 4, 3, 5}, true},
+		{1, []int{0, 1, 4, 3, 5}, true},
+		{2, []int{0, 1, 4, 3, 5}, false},
+		{3, []int{0, 1, 4, 3, 5}, true},
+		{4, []int{0, 1, 4, 3, 5}, true},
+		{5, []int{0, 1, 4, 3, 5}, true},
+		{6, []int{0, 1, 4, 3, 5}, false},
+		{7, []int{0, 1, 4, 3, 5}, false},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%d", tt.val)
+		t.Run(testname, func(t *testing.T) {
+			ans := InIntList(tt.val, tt.list)
+			if ans != tt.want {
+				t.Errorf("got `%t`, want `%t`", ans, tt.want)
 			}
 		})
 	}
